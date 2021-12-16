02d2ec09bfd1ba0fa005dcd407c058a6b899acff
Levente Nas <levente.nas@gmail.com>
2021-12-16 17:03:55 +0100

    added source and computer vision files

---
+++ computer_vision/ImageProcessor.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "ImageProcessor.hpp"
+
+class ImageProcessor {
+
+
+};
---
+++ src/Source.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+#include <torch/torch.h>
+
+#include "ImageProcessor.hpp"
+
+int main() {
+
+    
+}
