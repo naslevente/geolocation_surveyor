6ae0c0e309e89cf7cf7e825c81312b6008950f70
Levente Nas <levente.nas@gmail.com>
2022-03-26 11:10:56 +0000

    Added sensor and motor control configs

--- tools/drive.py
+++ tools/drive.py
@@ -6,7 +6,30 @@ from picamera import PiCamera
 import time
 from PIL import Image
 
+#Motor and proximity sensor imports
+import signal
+import RPi.GPIO as MotorGPIO
+import RPi.GPIO as ProximityGPIO
+
+#Servo imports
+from gpiozero import Servo
+
+#**ESC ARMING IS A MUST BEFORE RUNNING THIS SCRIPT**
 if __name__ == "__main__":
+    #Setting proximity sensor configs
+    ProximityGPIO.setmode(ProximityGPIO.BCM)
+    ProximityGPIO.setup(25, ProximityGPIO.IN)
+
+    #Setting up motor configs
+    MotorGPIO.setwarnings(False)
+    MotorGPIO.setmode(MotorGPIO.BCM)
+    MotorGPIO.setup(27, MotorGPIO.OUT)
+    MotorPWM = MotorGPIO.PWM(27, 50)
+
+
+    #Setting up servos
+    steer_servo = Servo(27)
+    break_servo = Servo(17)
 
     # get path to computer vision executable
     parser = argparse.ArgumentParser()
@@ -21,13 +44,50 @@ if __name__ == "__main__":
     #pathToVid = args.video
     pathToData = args.data_location
 
-    # capture first and second frame and store at given location
-    camera = PiCamera()
-    camera.capture(pathToData + "/test_image_frame_one.jpg")
-    time.sleep(3) # wait a number of seconds before taking second frame
-    camera.capture(pathToData + "/test_image_frame_two.jpg")
+    idx = 0
 
-    # run executable with input frames and extract output delta angle
-    pathToFirstFrame = pathToData + "/test_image_frame_one.jpg"
-    pathToSecondFrame = pathToData + "/test_image_frame_two.jpg"
-    output_angle = os.system(pathToExe + " " + pathToFirstFrame + " " + pathToSecondFrame)
+    try:
+
+        while True:
+            # **Do NOT forget to arm ESC before this**
+            # TODO: enter here motor starting/continuing to work
+            MotorPWM.start(0)
+            MotorPWM.ChangeDutyCycle(8)
+
+            # capture first and second frame and store at given location
+            camera = PiCamera()
+            camera.capture(pathToData + "/test_image_frame_" + idx + ".jpg")
+            pathToFirstFrame = pathToData + "/test_image_frame_" + idx + ".jpg"
+            print("captured first frame | " + pathToFirstFrame)
+            idx = idx + 1
+
+            time.sleep(3) # wait a number of seconds before taking second frame
+            camera.capture(pathToData + "/test_image_frame_" + idx + ".jpg")
+            pathToSecondFrame = pathToData + "/test_image_frame_" + idx + ".jpg"
+            print("captured second frame | " + pathToSecondFrame)
+            idx = idx + 1
+
+            # run executable with input frames and extract output delta angle
+            output_angle = os.system(pathToExe + " " + pathToFirstFrame + " " + pathToSecondFrame)
+
+            #Proximity sensor output
+            if GPIO.input(25):
+                print("NO COLLISION")
+            else:
+                print("COLLISION")
+            time.sleep(0.5)
+
+            # TODO: enter here change in steering based off output
+            steer_servo.value = ANGLE_BETWEEN_1_AND_MINUS_1
+            #time.sleep(3) or however long it should wait until next capture
+
+            #Breaking servo activated
+            break_servo.value = -0.5
+
+            #Stopping motor
+            MotorPWM.ChangeDutyCycle(0)
+
+    except KeyboardInterrupt:
+        #Cleanup GPIO config
+        GPIO.cleanup()
+        print("---Geolocation Surveyor test run ended---")
